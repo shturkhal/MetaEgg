38fead0b655d94863de226b66ccc320c71507727
shturkhal <shturhal@gmail.com>
2026-02-27 15:49:06 +0200

    Initial Commit

---
+++ MetaEgg/MetaEggApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaEggApp.swift
+//  MetaEgg
+//
+//  Created by Ivan Shturkhal on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MetaEggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
